7a3d55ed77a0d289c8cf47e1762a8ac1b9f5d545
Wes Galbraith <galbwe92@gmail.com>
2020-01-18 19:13:50 -0700

    Changed App from a functional to a class component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>It works!</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>It works!</h1>
+        <h2>Hooray!</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
