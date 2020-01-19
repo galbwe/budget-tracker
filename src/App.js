bbcb81f033ac6d9ed7a4209af4a70ab277ed9c74
Wes Galbraith <galbwe92@gmail.com>
2020-01-18 19:39:31 -0700

    Added state to App component, and display current month and year.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import React from 'react';
 
+const MONTH_NAMES = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+    let initialMonth = 1; //HARD CODED
+    let initialYear = 2020; //HARD CODED
+    this.state = {
+      displayMonth: initialMonth,
+      displayYear: initialYear,
+      monthlyBudgets: [
+        {
+          month: initialMonth,
+          year: initialYear,
+          budget: 0,
+          expenses: [
+          {/*
+            {
+              day: 'integer',
+              amount: 'numeric' > 0,
+              description: 'string'
+            },
+            ...
+          */}
+        ]
+           }
+      ]
+    }
+
+    this.getMonthName = this.getMonthName.bind(this);
+  }
+
+  getMonthName(monthNumber) {
+    return MONTH_NAMES[monthNumber - 1];
   }
 
   render() {
     return (
       <div className="App">
-        <h1>It works!</h1>
-        <h2>Hooray!</h2>
+        <h1>{this.getMonthName(this.state.displayMonth)} {this.state.displayYear}</h1>
       </div>
     );
   }
