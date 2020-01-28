79567faed69cc810c0ee64e1004d0b08f2a7065a
Wes Galbraith <galbwe92@gmail.com>
2020-01-28 10:41:45 -0700

    added post methods for expense and monthly budget endpoints

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const api = express();
 const models = require('./models.js');
 console.log(models);
 const port = 8080;
+
+const jsonParser = bodyParser.json();
 
 api.get('/test', (req, res) => {
   res.send('It works!');
@@ -10,14 +13,31 @@ api.get('/test', (req, res) => {
 
 api.get('/api/v1/expenses', (req, res) => {
   models.Expense.find((err, expenses) => {
-    res.send(JSON.stringify(expenses));
+    res.json(expenses);
   })
 })
 
 api.get('/api/v1/monthly-budgets', (req, res) => {
-  models.MothlyBudget.find((err, budgets) => {
-    res.send(JSON.stringify(budgets));
+  models.MonthlyBudget.find((err, budgets) => {
+    res.json(expenses);
   })
 })
+
+api.post('/api/v1/expenses', jsonParser, (req, res) => {
+  expense = new models.Expense(req.body);
+  expense.save();
+  let data = req.body;
+  data.id = expense._id;
+  res.send(data);
+})
+
+api.post('/api/v1/monthly-budgets', jsonParser, (req, res) => {
+  budget = new models.MonthlyBudget(req.body);
+  budget.save();
+  let data = req.body;
+  data.id = budget._id;
+  res.send(data);
+})
+
 
 api.listen(port, () => console.log('API listening on port ' + port));
