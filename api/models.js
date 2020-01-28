26ccff3073994eefed0cf9de1a34bd88c31a3529
Wes Galbraith <galbwe92@gmail.com>
2020-01-28 09:24:47 -0700

    added models

---
+++ api/models.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/budget_tracker', { useNewUrlParser: true });
+
+const expenseSchema = new mongoose.Schema({
+  month: Number,
+  day: Number,
+  year: Number,
+  amount: Number,
+  description: String,
+});
+
+const Expense = mongoose.model('Expense', expenseSchema);
+
+const monthlyBudgetSchema = new mongoose.Schema({
+  month: Number,
+  year: Number,
+  budget: Number,
+});
+
+const MonthlyBudget = mongoose.model('MonthlyBudget', monthlyBudgetSchema);
