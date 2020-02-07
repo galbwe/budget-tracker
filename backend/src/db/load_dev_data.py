14f6f0daba48e51807024005cdebf998f8c3488b
Wes Galbraith <galbwe92@gmail.com>
2020-02-07 13:57:25 -0700

    Add read_csv utility function

--- backend/src/db/load_dev_data.py
+++ backend/src/db/load_dev_data.py
@@ -1,17 +1,21 @@
 #! python3
 import os
-import csv
 import openpyxl
 import psycopg2
 
 print(__name__)
 
 from ..utils.find_files import find_files
+from ..utils.read_csv import read_csv
 
 # find all csvs in data folder
 dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
 csvs = find_files(dir, 'csv')
 # read data from each csv
+data = []
+for csv in csvs:
+    data.extend(read_csv(csv, floats=['amount'], dates=['date'])[1:])
+print(data)
 
 # find all xlsx files in data folder
 xlsxs = find_files(dir, 'xlsx')
---
+++ backend/src/utils/read_csv.py
@@ -0,0 +1,39 @@
+import csv
+from datetime import datetime
+
+def read_csv(file, floats=None, dates=None):
+    '''Reads a csv file to a list of tuples
+    The first row of the spreadsheet is assumed to contain headers
+    The floats argument gives a list of column names that are of the float datatype
+    The dates argument gives a list of column names that are of the datetime datatype
+        dates are assumed to be specified in the format d/m/yyyy
+    '''
+    with open(file) as f:
+        reader = csv.reader(f)
+        headers = next(reader)
+        float_columns = None
+        if floats:
+            float_columns = [i for i, head in enumerate(headers) if head in floats]
+        date_columns = None
+        if dates:
+            date_columns = [i for i, head in enumerate(headers) if head in dates]
+        result = [tuple(headers)]
+        for row in reader:
+            parsed_row = []
+            print(row)
+            for i, e in enumerate(row):
+                if i in float_columns:
+                    parsed_row.append(float(e))
+                elif i in date_columns:
+                    parsed_row.append(datetime.strptime(e, '%m/%d/%Y'))
+                else:
+                    parsed_row.append(e)
+            result.append(tuple(parsed_row))
+        return result
+
+
+if __name__ == '__main__':
+    # test
+    file = '../db/data/dev-data.csv'
+    data = read_csv(file, floats=['amount'], dates=['date'])
+    print(data)
