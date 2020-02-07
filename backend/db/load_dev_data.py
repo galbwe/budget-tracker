bdd5dfbf7e8b0e4eb2b14880900fdb3d531e603b
Wes Galbraith <galbwe92@gmail.com>
2020-02-07 10:22:40 -0700

    added development data

---
+++ backend/db/load_dev_data.py
@@ -0,0 +1,15 @@
+#! python3
+import os
+import csv
+import openpyxl
+import psycopg2
+
+# find all csvs in data folder
+
+# read data from each csv
+
+# find all xlsx files in data folder
+
+# read data from each xlsx
+
+# write data to database
